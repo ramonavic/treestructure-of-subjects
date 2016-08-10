86cfbd7fc7d92b6c4aa39a5f56fa59c8c052caa8
ramonavic <ramon.hofmeister@gmail.com>
2016-08-10 15:13:31 +0200

    Subject class and find functions

--- src/containers/theme.js
+++ src/containers/theme.js
@@ -12,28 +12,14 @@ class Theme extends React.Component {
     }
 }
 
-  componentDidMount() {
-    let topSubjectsArray = this.state.topSubjects;
-    let subSubjectsArray = this.state.subSubjects;
 
-    console.log(topSubjectsArray.length)
-
-    for(var i = 0; i < topSubjectsArray.length; i++) {
-      for(var j = 0; j < subSubjectsArray.length; j++) {
-        if (subSubjectsArray.parent_subject_id[j] == topSubjectsArrays.id[i]) {
-          console.log("Addewareed", subSubjects.parent_subject.id[j])
-
-        }
-      }
-    }
-  }
 
   render() {
     let component = this
 
     return(
       <div>
-      
+
       </div>
     )
   }
